09a11719739b0faeb452a5a047b642c89d6b31f5
PotatoFleet <supsrasri@gmail.com>
2022-04-02 11:07:36 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const cors = require("cors");
+const mongoose = require("mongoose");
+const Item = require("./models/Item");
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.use(cors());
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost/db");
+
+app.get("/", async (_req, res) => {
+  const items = await Item.find({});
+  res.render("index", { data: items.reverse() });
+});
+
+app.post("/new-idea", (req, res) => {
+  new Item({ title: req.body.title, description: req.body.description }).save();
+  res.send(true);
+});
+
+app.listen(PORT);
